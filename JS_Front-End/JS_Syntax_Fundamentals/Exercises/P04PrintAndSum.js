bcafb259e550b80de4615c313b787912b78914ea
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-02-24 02:38:47 +0200

    2023-02-24_JS_Syntaxis

---
+++ JS_Front-End/JS_Syntax_Fundamentals/Exercises/P04PrintAndSum.js
@@ -0,0 +1,14 @@
+function slove(startNum, endNum){
+let sumNumbers=0;
+let arrNumbers = [];
+
+    for (let i = startNum; i <= endNum; i++) {
+        arrNumbers.push(i);
+        sumNumbers += i;
+    }
+
+    console.log(arrNumbers.join(" "));
+    console.log("Sum: "+sumNumbers);
+}
+
+slove(5, 10);
---
+++ JS_Front-End/JS_Syntax_Fundamentals/Exercises/P05MultiplicationTable.js
@@ -0,0 +1,9 @@
+function slove(number){
+
+    for (let i = 1; i <= 10; i++) {
+    
+        console.log(number + " X " + i + " = " + number*i);
+    }
+}
+
+slove(5);
