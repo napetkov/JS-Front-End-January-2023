835bd8efe785b55b12720d145ca2bfdc2183c260
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-02-27 12:27:38 +0200

    2023-02-27_JS_Syntax_Fundamentals_More-Exercises

--- JS_Front-End/JS_Syntax_Fundamentals/Exercises/P20Pascal-Case Splitter.js
+++ JS_Front-End/JS_Syntax_Fundamentals/Exercises/P20Pascal-Case Splitter.js
@@ -1,21 +1,21 @@
 function pascalCaseSplitter(text) {
   let startIndexOfWord = 0;
 
-  let arrayWithWordsUpperCase = [];
+  let arrayWithWordsUpperCase = text.split(/(?=[A-Z])/g);
 
-  for (let i = 1; i < text.length; i++) {
-    let charAtIndexASCII = text.charCodeAt(i);
+  // for (let i = 1; i < text.length; i++) {
+  //   let charAtIndexASCII = text.charCodeAt(i);
 
-    if (charAtIndexASCII >= 65 && charAtIndexASCII <= 90) {
-      let wordStartWithUpperCase = text.substring(startIndexOfWord, i);
-      arrayWithWordsUpperCase.push(wordStartWithUpperCase);
-      startIndexOfWord = i;
-    }
-  }
-    arrayWithWordsUpperCase.push(text.slice(startIndexOfWord));
+  //   if (charAtIndexASCII >= 65 && charAtIndexASCII <= 90) {
+  //     let wordStartWithUpperCase = text.substring(startIndexOfWord, i);
+  //     arrayWithWordsUpperCase.push(wordStartWithUpperCase);
+  //     startIndexOfWord = i;
+  //   }
+  // }
+  //   arrayWithWordsUpperCase.push(text.slice(startIndexOfWord));
 
   console.log(arrayWithWordsUpperCase.join(", "));
 }
 
-// pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
-pascalCaseSplitter("avatar");
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
+// pascalCaseSplitter("Avatar");
