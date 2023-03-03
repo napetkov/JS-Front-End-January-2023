a808b97a27f89a847ae34a8e4238b567184e59a2
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-03-03 08:59:35 +0200

    JS-Front-End-Functions-and-Statements-Lab-P02/03

--- JS_Front-End/Functions_and_Statements/Lab/P01FormatGrade.js
+++ JS_Front-End/Functions_and_Statements/Lab/P01FormatGrade.js
@@ -1,26 +1,25 @@
-function formatGrade(grade){
-    let textGrade = '';
-    if(grade < 3){
-        textGrade = 'Fail';
-    }else if(grade >= 3 && grade < 3.50){
-        textGrade = 'Poor';
-    }else if(grade >= 3.50 && grade < 4.50){
-        textGrade = 'Good';
-    }else if(grade >= 4.50 && grade < 5.50){
-        textGrade = 'Very good';
-    }else if(grade >= 5.50){
-        textGrade = 'Excellent';
-    }
+function formatGrade(grade) {
+  let textGrade = "";
+  if (grade < 3) {
+    textGrade = "Fail";
+  } else if (grade >= 3 && grade < 3.5) {
+    textGrade = "Poor";
+  } else if (grade >= 3.5 && grade < 4.5) {
+    textGrade = "Good";
+  } else if (grade >= 4.5 && grade < 5.5) {
+    textGrade = "Very good";
+  } else if (grade >= 5.5) {
+    textGrade = "Excellent";
+  }
 
-    
-    function printGrade(){
-        if(grade < 3){
-            console.log(`${textGrade} (2)`);
-        }else{
-        console.log(`${textGrade} (${grade.toFixed(2)})`);
+  function printGrade() {
+    if (grade < 3) {
+      console.log(`${textGrade} (2)`);
+    } else {
+      console.log(`${textGrade} (${grade.toFixed(2)})`);
     }
-    };
-    printGrade(grade,textGrade);
+  }
+  printGrade(grade, textGrade);
 }
 
 formatGrade(2.99);
