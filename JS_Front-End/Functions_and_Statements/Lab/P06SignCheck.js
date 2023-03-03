34c52192995f0e075e757ae65e91babd8d31a6cb
Nikolay Petkov <106302204+napetkov@users.noreply.github.com>
2023-03-03 11:22:31 +0200

    JS-Front-End-Functions-and-Statements-Lab-P6 - new variant

--- JS_Front-End/Functions_and_Statements/Lab/P06SignCheck.js
+++ JS_Front-End/Functions_and_Statements/Lab/P06SignCheck.js
@@ -1,10 +1,13 @@
 function signCheck(...numbers) {
-  let negativeNumbersArr = numbers.filter((num) => num < 0);
+  //   let negativeNumbersArr = numbers.filter((num) => num < 0);
 
-  if (negativeNumbersArr.length % 2 === 0) {
-    return "Positive";
-  } else {
-    return "Negative";
-  }
+  //   if (negativeNumbersArr.length % 2 === 0) {
+  //     return "Positive";
+  //   } else {
+  //     return "Negative";
+  //   }
+  return numbers
+  .filter((num) => num < 0)
+  .length % 2 === 0 ?'Positive' :'Negative';
 }
 console.log(signCheck(-1, -2, -3));
